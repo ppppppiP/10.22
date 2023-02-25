c09115fde5d4002895b9f685c98184d4f1687ad0
sarpi <sarpi@SARPINSKIY>
2023-02-25 14:34:54 +0300

    Добавьте файлы проекта.

---
+++ 10.22/Program.cs
@@ -0,0 +1,33 @@
+﻿static int Max1(int a, int b)
+{
+    if (a > b)
+    {
+        return a;
+    }
+    else
+    {
+        return b;
+    }
+}
+
+Console.WriteLine("Введите 6 чисел: ");
+int[] arr = new int[6];
+
+for(int a = 0; a< arr.Length;a++)
+{
+    arr[a] = Convert.ToInt32(Console.ReadLine());
+}
+
+
+if (Max1(arr[0], arr[1])>Max1(arr[2], arr[3])&& Max1(arr[0], arr[1])> Max1(arr[4], arr[5]))
+{
+    Console.WriteLine("Максимальное число: " + Max1(arr[0], arr[1]));
+
+}else if(Max1(arr[2], arr[3])> Max1(arr[0], arr[1])&& Max1(arr[2], arr[3])> Max1(arr[4], arr[5]))
+{
+    Console.WriteLine("Максимальное число: " + Max1(arr[2], arr[3]));
+
+}else if(Max1(arr[4], arr[5])> Max1(arr[0], arr[1])&& Max1(arr[4], arr[5])> Max1(arr[2], arr[3]))
+{
+    Console.WriteLine("Максимальное число: "+Max1(arr[4], arr[5]));
+}
